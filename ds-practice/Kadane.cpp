8a629787c3a2ecfb3f43934a6c89ddc206ba8aa3
adityajunwal <adityakjunwal@gmail.com>
2025-07-01 20:22:08 +0530

    added Kadane's Algorithm in Python and C++

--- ds-practice/Kadane.cpp
+++ ds-practice/Kadane.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 using namespace std;
 
+// Naive / Brute Force approach
 int maxSubArray(int arr[], int n){
     int res = arr[0];
 
@@ -14,9 +15,26 @@ int maxSubArray(int arr[], int n){
             res = max(current, res);
         }
     }
+    return res;
+}
+
+int kadane (int arr[], int n) {
+    int res = arr[0];
+    int maxEnd = arr[0];
+
+    for (int i=1; i<n; i++) {
+        maxEnd = max(arr[i], maxEnd + arr[i]);
+
+        res = max(maxEnd, res);
+    }
+
+    return res;
 }
 
 int main() {
     int arr[] = {2, 3, -8, 7, -1, 2, 3};
     int result = maxSubArray(arr, 7);
+    int resultKD = kadane(arr, 7);
+    cout << "Max Subarray sum via Brute Force: " << result << endl;
+    cout << "Max Subarray sum via Kadane's Algorithm: " << resultKD << endl;
 }
--- ds-practice/Kadane.py
+++ ds-practice/Kadane.py
@@ -12,8 +12,22 @@ def maxSubArraySum(arr):
 
     return res
 
+
+def Kadane(arr):
+    res = arr[0]
+    maxEnd = arr[0]
+
+    for i in range(1, len(arr)):
+        maxEnd = max(arr[i], maxEnd + arr[i])
+
+        res = max(maxEnd, res)
+
+    return res
+
+
 # arr = [2, 3, -8, 7, -1, 2, 3]
 arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-print(maxSubArraySum(arr))
+# print(maxSubArraySum(arr))
+print(Kadane(arr))
 
 # print(6 << 1) # 110 => 1100 
