2701529b3d873645e489e4d92c0e6941e53ae1df
adityajunwal <adityakjunwal@gmail.com>
2025-07-01 14:02:56 +0530

    zz

--- phase2/1jul.cpp
+++ phase2/1jul.cpp
@@ -1,23 +1,41 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int main() {
 
-    int arr[] = {10, 20, 30, 40, 50};
+int maxSubArraySum(int arr[], int n) {
+    int res = arr[0];
 
-    for (int i=0; i<5; i++) {
-        for (int j=i; j<5; j++) {
+    for (int i=0; i<n; i++) {
+        int current = 0;
+
+        for (int j=i; j<n; j++) {
+            current += arr[j];
+
+            res = max(current, res);
+        }
+    }
+
+    return res;
+}
+
+
+void printSubArray(int arr[], int n) {
+
+    for (int i=0; i<n; i++) {
+        for (int j=i; j<n; j++) {
             bool first = true;
             for (int k=i; k<=j; k++) {
                 if (first) {cout << "[" << arr[k]; first = false;}
-
                 else {cout << ", " << arr[k]; }
             } cout << "]" << endl;
         } cout << endl;
     }
 }
 
-/*
 
-
-*/
+int main() {
+    int arr[] = {-2, -1, -3, -4, -1, -2, -1, 77, -5, -4};
+    int n = sizeof(arr)/sizeof(int);
+    cout << maxSubArraySum(arr, n);
+}
