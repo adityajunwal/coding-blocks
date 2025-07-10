27cbb01bb192e6ef168873ec0df04b0134869b3a
adityajunwal <adityakjunwal@gmail.com>
2025-07-10 10:07:55 +0530

    Final Commit 10 july

--- morning-lectures/10jul.cpp
+++ morning-lectures/10jul.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 // Counting sort
-int countingSOrt(int arr[], int n, int k){
+// int countingSOrt(int arr[], int n, int k){
     // unordered_map<int, int> counts;
     
     // for (int i=0; i<n; i++){
@@ -19,33 +19,42 @@ int countingSOrt(int arr[], int n, int k){
     //     }
     // }
  
-    int count[k+1] = {0};
+//     int count[k+1] = {0};
 
-    for (int i=0; i<n; i++){
-        count[arr[i]]++;
+//     for (int i=0; i<n; i++){
+//         count[arr[i]]++;
+//     }
+
+//     int index = 0;
+//     for (int i=0; i<=k; i++){
+//         while (count[i] > 0){
+//             arr[index++] = i;
+//             count[i]--;
+//         }
+//     }
+// }
+
+
+// int main(){
+//     // int x = 10;
+//     // int *xptr = &x;
+//     // cout << xptr << endl;
+//     // xptr++;
+//     // cout << xptr << endl;
+
+//     int arr[] = {1, 0, 3, 2, 3, 1, 2, 0, 2};
+//     countingSOrt(arr, 9, 3);
+
+//     for (int x: arr){
+//         cout << x << " ";
+//     } cout << endl;
+// }
+
+int main() {
+    int arr[3] = {1,2,3};
+    int *ptr = &arr[0];
+    for (int i=0; i<3; i++){
+        cout << *ptr << " ";
+        ptr++;
     }
-
-    int index = 0;
-    for (int i=0; i<=k; i++){
-        while (count[i] > 0){
-            arr[index++] = i;
-            count[i]--;
-        }
-    }
-}
-
-
-int main(){
-    // int x = 10;
-    // int *xptr = &x;
-    // cout << xptr << endl;
-    // xptr++;
-    // cout << xptr << endl;
-
-    int arr[] = {1, 0, 3, 2, 3, 1, 2, 0, 2};
-    countingSOrt(arr, 9, 3);
-
-    for (int x: arr){
-        cout << x << " ";
-    } cout << endl;
 }
